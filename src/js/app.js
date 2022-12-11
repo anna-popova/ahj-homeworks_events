2941ecf2ed7107d1597d4798679880a47c81aec2
Anna Popova <panasenko-anna.8@mail.ru>
2022-12-11 16:36:33 +0300

    solve task

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,10 @@
 import PlayingArea from '../components/playing-area/playing-area';
+import userClick from '../components/user-click/user-click';
 
 document.addEventListener('DOMContentLoaded', () => {
   const playingArea = new PlayingArea(document.querySelector('.playing-area'));
 
-  setInterval(playingArea.randomMovingGoblin, 1000);
+  setInterval(playingArea.randomMovingGoblin, 2000);
+
+  userClick();
 })
