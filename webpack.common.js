2e3ec508e69e60ecd9e1c890d86bc28619756b95
Anna Popova <panasenko-anna.8@mail.ru>
2022-12-11 15:36:13 +0300

    fix build errors

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,6 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   target: 'web',
-  devtool: 'sourse-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '',
